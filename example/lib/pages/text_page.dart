d9ead2d2b30c6114ec4ee5434323490746e70271
BoseVJeff <jeffbvic003@gmail.com>
2025-06-26 12:04:53 +0530

    Updated text demo to include font weights and styles (normal/italic)

--- example/lib/pages/text_page.dart
+++ example/lib/pages/text_page.dart
@@ -11,52 +11,141 @@ class TextPage extends StatefulWidget {
 
 class _TextPageState extends State<TextPage> {
   late List<BoxShadow> textShadow;
+  late FontWeight weight;
+  late bool isItalic;
 
   @override
   void initState() {
     super.initState();
     textShadow = TwShadow.md;
+    weight = TwFontWeightRaw.medium;
+    isItalic = false;
   }
 
   @override
   Widget build(BuildContext context) {
     return StaticDynamicListView(
       children: [
-        SegmentedButton<List<BoxShadow>>(
-          segments: [
-            ButtonSegment(
-              value: TwTextShadow.xs2,
-              label: Text("2xs"),
-              enabled: textShadow != TwTextShadow.xs2,
-            ),
-            ButtonSegment(
-              value: TwTextShadow.xs,
-              label: Text("xs"),
-              enabled: textShadow != TwTextShadow.xs,
-            ),
-            ButtonSegment(
-              value: TwTextShadow.sm,
-              label: Text("sm"),
-              enabled: textShadow != TwTextShadow.sm,
-            ),
-            ButtonSegment(
-              value: TwTextShadow.md,
-              label: Text("md"),
-              enabled: textShadow != TwTextShadow.md,
-            ),
-            ButtonSegment(
-              value: TwTextShadow.lg,
-              label: Text("lg"),
-              enabled: textShadow != TwTextShadow.lg,
-            ),
-          ],
-          selected: {textShadow},
-          onSelectionChanged: (p0) {
-            Set<List<BoxShadow>> diff = p0.difference({textShadow});
-            setState(() {
-              textShadow = diff.first;
-            });
-          },
+        Padding(
+          padding: TwPadding.p(2),
+          child: SegmentedButton<FontWeight>(
+            showSelectedIcon: false,
+            multiSelectionEnabled: false,
+            segments: [
+              ButtonSegment(
+                value: TwFontWeightRaw.thin,
+                label: Text("thin"),
+                enabled: weight != TwFontWeightRaw.thin,
+              ),
+              ButtonSegment(
+                value: TwFontWeightRaw.extralight,
+                label: Text("extralight"),
+                enabled: weight != TwFontWeightRaw.extralight,
+              ),
+              ButtonSegment(
+                value: TwFontWeightRaw.light,
+                label: Text("light"),
+                enabled: weight != TwFontWeightRaw.light,
+              ),
+              ButtonSegment(
+                value: TwFontWeightRaw.normal,
+                label: Text("normal"),
+                enabled: weight != TwFontWeightRaw.normal,
+              ),
+              ButtonSegment(
+                value: TwFontWeightRaw.medium,
+                label: Text("medium"),
+                enabled: weight != TwFontWeightRaw.medium,
+              ),
+              ButtonSegment(
+                value: TwFontWeightRaw.semibold,
+                label: Text("semibold"),
+                enabled: weight != TwFontWeightRaw.semibold,
+              ),
+              ButtonSegment(
+                value: TwFontWeightRaw.bold,
+                label: Text("bold"),
+                enabled: weight != TwFontWeightRaw.bold,
+              ),
+              ButtonSegment(
+                value: TwFontWeightRaw.extrabold,
+                label: Text("extrabold"),
+                enabled: weight != TwFontWeightRaw.extrabold,
+              ),
+              ButtonSegment(
+                value: TwFontWeightRaw.black,
+                label: Text("black"),
+                enabled: weight != TwFontWeightRaw.black,
+              ),
+            ],
+            selected: {weight},
+            onSelectionChanged: (Set<FontWeight> w) {
+              Set<FontWeight> diff = w.difference({weight});
+              setState(() {
+                weight = diff.first;
+              });
+            },
+          ),
+        ),
+        Padding(
+          padding: TwPadding.p(2),
+          child: SegmentedButton<List<BoxShadow>>(
+            showSelectedIcon: false,
+            multiSelectionEnabled: false,
+            segments: [
+              ButtonSegment(
+                value: [],
+                label: Text("none"),
+                enabled: textShadow.isNotEmpty,
+              ),
+              ButtonSegment(
+                value: TwTextShadow.xs2,
+                label: Text("2xs"),
+                enabled: textShadow != TwTextShadow.xs2,
+              ),
+              ButtonSegment(
+                value: TwTextShadow.xs,
+                label: Text("xs"),
+                enabled: textShadow != TwTextShadow.xs,
+              ),
+              ButtonSegment(
+                value: TwTextShadow.sm,
+                label: Text("sm"),
+                enabled: textShadow != TwTextShadow.sm,
+              ),
+              ButtonSegment(
+                value: TwTextShadow.md,
+                label: Text("md"),
+                enabled: textShadow != TwTextShadow.md,
+              ),
+              ButtonSegment(
+                value: TwTextShadow.lg,
+                label: Text("lg"),
+                enabled: textShadow != TwTextShadow.lg,
+              ),
+            ],
+            selected: {textShadow},
+            onSelectionChanged: (p0) {
+              Set<List<BoxShadow>> diff = p0.difference({textShadow});
+              setState(() {
+                textShadow = diff.first;
+              });
+            },
+          ),
+        ),
+        ListTile(
+          title: Text("Italic Text"),
+          onTap: () => setState(() {
+            isItalic = !isItalic;
+          }),
+          leading: Checkbox(
+            value: isItalic,
+            onChanged: (bool? newValue) {
+              setState(() {
+                isItalic = newValue ?? isItalic;
+              });
+            },
+          ),
         ),
         ListTile(
           title: Text("font-sans"),
@@ -67,7 +156,11 @@ class _TextPageState extends State<TextPage> {
           subtitleTextStyle: Theme.of(context).textTheme.labelMedium!
               .lg()
               .withSans()
-              .copyWith(shadows: textShadow),
+              .copyWith(
+                shadows: textShadow,
+                fontWeight: weight,
+                fontStyle: isItalic ? FontStyle.italic : FontStyle.normal,
+              ),
         ),
         ListTile(
           title: Text("font-serif"),
@@ -78,7 +171,11 @@ class _TextPageState extends State<TextPage> {
           subtitleTextStyle: Theme.of(context).textTheme.labelMedium!
               .lg()
               .withSerif()
-              .copyWith(shadows: textShadow),
+              .copyWith(
+                shadows: textShadow,
+                fontWeight: weight,
+                fontStyle: isItalic ? FontStyle.italic : FontStyle.normal,
+              ),
         ),
         ListTile(
           title: Text("font-mono"),
@@ -89,7 +186,11 @@ class _TextPageState extends State<TextPage> {
           subtitleTextStyle: Theme.of(context).textTheme.labelMedium!
               .lg()
               .withMono()
-              .copyWith(shadows: textShadow),
+              .copyWith(
+                shadows: textShadow,
+                fontWeight: weight,
+                fontStyle: isItalic ? FontStyle.italic : FontStyle.normal,
+              ),
         ),
       ],
     );
